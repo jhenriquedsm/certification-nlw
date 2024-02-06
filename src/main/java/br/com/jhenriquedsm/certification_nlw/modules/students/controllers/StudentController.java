287b303a0c2cfb04c07b6548aecc8745ca1ef167
jhenriquedsm <jhsilvamata@gmail.com>
2024-02-06 14:21:51 -0300

    Aula 1: created firts controllers, dto, entities and useCases

--- src/main/java/br/com/jhenriquedsm/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/br/com/jhenriquedsm/certification_nlw/modules/students/controllers/StudentController.java
@@ -4,18 +4,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jhenriquedsm.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.jhenriquedsm.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+    // precisa usar o USECASE
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
     
     @PostMapping("/verifyIfHasCertification")
     public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO){
-
-        System.out.println(verifyHasCertificationDTO);
+        var result = this.verifyIfHasCertificationUseCase.execute(verifyHasCertificationDTO);
+        if (result){
+            return "Usuário já fez a prova";
+        }
         return "Usuário pode fazer a prova";
     }
 }
