24dedf737a942318a7a210f0dd981bc693dbf759
jhenriquedsm <jhsilvamata@gmail.com>
2024-02-06 13:43:57 -0300

    primeiro post

--- src/main/java/br/com/jhenriquedsm/certification_nlw/PrimeiraController.java
+++ src/main/java/br/com/jhenriquedsm/certification_nlw/PrimeiraController.java
@@ -3,6 +3,7 @@ package br.com.jhenriquedsm.certification_nlw;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/primeiraController")
@@ -12,6 +13,11 @@ public class PrimeiraController {
     public Usuario retornoPrimeiraController(){
         var user = new Usuario("José Henrique", 19);
         return user;
+    }
+
+    @PostMapping("primeiroPost")
+    public String primeiroPost() {
+        return "Meu primeiro post";
     }
 
     record Usuario(String nome, int idade){}
