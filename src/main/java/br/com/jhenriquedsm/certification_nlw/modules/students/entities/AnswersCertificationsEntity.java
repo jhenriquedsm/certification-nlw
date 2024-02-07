13c24990cd02bf7cde7c6065f4918fadda41ddfb
jhenriquedsm <jhsilvamata@gmail.com>
2024-02-06 21:04:58 -0300

    persistence of the entities

--- src/main/java/br/com/jhenriquedsm/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
+++ src/main/java/br/com/jhenriquedsm/certification_nlw/modules/students/entities/AnswersCertificationsEntity.java
@@ -1,7 +1,17 @@
 package br.com.jhenriquedsm.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +19,36 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswersCertificationsEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @JoinColumn(name = "certification_id")
     private UUID certification_id;
+
+    @ManyToOne
+    @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    private CertificationsStudentEntity certificationsStudentEntity;
+
+    @Column(name = "student_id")
     private UUID student_id;
-    private UUID questions_id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @Column(name = "question_id")
+    private UUID question_id;
+
+    @Column(name = "answer_id")
     private UUID answer_id;
+
+    @Column(name = "is_correct")
     private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
